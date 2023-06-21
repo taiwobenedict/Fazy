086c51cec9b0d4f67b4f68090fa1118d401877b0
Taiwo <ajayitaiwotunta@gmail.com>
2023-06-21 09:32:00 +0100

    Initial commit

--- app.js
+++ app.js
@@ -6,4 +6,32 @@ var swiper = new Swiper(".mySwiper", {
     delay: 2500,
     disableOnInteraction: false,
   },
+
+  breakpoints: {
+    786: {
+      slidesPerView: 3,
+      spaceBetween: 5,
+      loop: true,
+      autoplay: {
+        delay: 2500,
+      }
+    }
+  }
 });
+
+
+// function myFunc(x) {
+//   if (x.matches) {
+//     let swiper = new Swiper(".mySwiper", {
+//       spaceBetween: 10,
+//       slidesPerView: 3,
+//       loop: true,
+//       autoplay: {
+//         delay: 2500,
+//         disableOnInteraction: false,
+//       },
+//     });
+//   }
+// }
+
+// var x = window.matchMedia("(min-width: 786)")
